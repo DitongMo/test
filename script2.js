a5284827d65a82e8d75bed8156394977d52bbd7a
DitongMo <77713981+DitongMo@users.noreply.github.com>
2023-07-30 19:54:28 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,49 +1,85 @@
+// Define the dimensions of the chart
+const margin = { top: 20, right: 20, bottom: 40, left: 60 };
+const width = 800 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+
+// Create the SVG element
+const svg = d3.select('chart2')
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
 // Load the data from CSV file
-d3.csv("US_vaccine_cummulative_trend.csv").then(function(data) {
-  // Convert date strings to Date objects
-  data.forEach(function(d) {
-    d.date = new Date(d.date);
-    d.covid_vaccine_administered = +d.covid_vaccine_administered;
+d3.csv('interactive_trend.csv').then(function(data) {
+  // Parse date
+  const parseDate = d3.timeParse('%m/%d/%Y');
+  data.forEach(d => {
+    d.Date = parseDate(d.Date);
+    d.Administered_Cumulative = +d.Administered_Cumulative;
+    d.Series_Complete_Cumulative = +d.Series_Complete_Cumulative;
+    d.Booster_Cumulative = +d.Booster_Cumulative;
   });
 
-  // Set up SVG container
-  var svg = d3.select("#chart2")
-    .append("svg")
-    .attr("width", "100%")
-    .attr("height", "100%")
-    .attr("viewBox", "0 0 800 400")
-    .append("g")
-    .attr("transform", "translate(50, 50)");
+  // Set the initial y-value
+  let yValue = 'Administered_Cumulative';
 
-  // Set up scales and axes
-  var xScale = d3.scaleTime()
-    .domain(d3.extent(data, function(d) { return d.date; }))
-    .range([0, 700]);
+  // Define the x and y scales
+  const xScale = d3.scaleTime()
+    .domain(d3.extent(data, d => d.Date))
+    .range([0, width]);
 
-  var yScale = d3.scaleLinear()
-    .domain([0, d3.max(data, function(d) { return d.covid_vaccine_administered; })])
-    .range([300, 0]);
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d[yValue])])
+    .range([height, 0]);
 
-  var xAxis = d3.axisBottom(xScale);
-  var yAxis = d3.axisLeft(yScale);
-
-  svg.append("g")
-    .attr("class", "x-axis")
-    .attr("transform", "translate(0, 300)")
-    .call(xAxis);
-
-  svg.append("g")
-    .attr("class", "y-axis")
-    .call(yAxis);
-
-  // Create a line generator
-  var line = d3.line()
-    .x(function(d) { return xScale(d.date); })
-    .y(function(d) { return yScale(d.covid_vaccine_administered); });
+  // Define the line functions
+  const line = d3.line()
+    .x(d => xScale(d.Date))
+    .y(d => yScale(d[yValue]));
 
   // Draw the line chart
-  svg.append("path")
+  svg.append('path')
     .datum(data)
-    .attr("class", "line")
-    .attr("d", line);
+    .attr('class', 'line')
+    .attr('d', line);
+
+  // Add x-axis
+  svg.append('g')
+    .attr('class', 'axis x-axis')
+    .attr('transform', `translate(0, ${height})`)
+    .call(d3.axisBottom(xScale));
+
+  // Add y-axis
+  svg.append('g')
+    .attr('class', 'axis y-axis')
+    .call(d3.axisLeft(yScale));
+
+  // Function to update the chart based on the selected y-value
+  function updateChart(selectedValue) {
+    yValue = selectedValue;
+
+    // Update the y scale domain
+    yScale.domain([0, d3.max(data, d => d[yValue])]);
+
+    // Update the line function
+    line.y(d => yScale(d[yValue]));
+
+    // Select the existing line and update the data
+    svg.selectAll('.line')
+      .datum(data)
+      .transition()
+      .attr('d', line);
+
+    // Update the y-axis
+    svg.select('.y-axis')
+      .transition()
+      .call(d3.axisLeft(yScale));
+  }
+
+  // Button event listeners
+  d3.select('#btnAdministered').on('click', () => updateChart('Administered_Cumulative'));
+  d3.select('#btnSeriesComplete').on('click', () => updateChart('Series_Complete_Cumulative'));
+  d3.select('#btnBooster').on('click', () => updateChart('Booster_Cumulative'));
 });
