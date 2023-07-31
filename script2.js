8f6fa1252dd28aa02756175d3929627ad16c5063
DitongMo <77713981+DitongMo@users.noreply.github.com>
2023-07-30 20:18:30 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,84 +1,86 @@
-// Define the dimensions of the chart
-const margin = { top: 20, right: 20, bottom: 40, left: 60 };
-const width = 800 - margin.left - margin.right;
-const height = 400 - margin.top - margin.bottom;
-
-// Create the SVG element
-const svg = d3.select('chart2')
-  .append('svg')
-  .attr('width', width + margin.left + margin.right)
-  .attr('height', height + margin.top + margin.bottom)
-  .append('g')
-  .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-// Load the data from CSV file
+// Load the data from CSV file and create the interactive line chart
 d3.csv('interactive_trend.csv').then(function(data) {
-  // Parse date
+  // Convert date string to Date object
+  const parseDate = d3.timeParse("%m/%d/%y");
   data.forEach(function(d) {
-    d.Date = new Date(d.Date);
+    d.Date = parseDate(d.Date);
     d.Administered_Cumulative = +d.Administered_Cumulative;
     d.Series_Complete_Cumulative = +d.Series_Complete_Cumulative;
     d.Booster_Cumulative = +d.Booster_Cumulative;
   });
 
-  // Set the initial y-value
-  let yValue = 'Administered_Cumulative';
+  // Set up the chart dimensions
+  const margin = { top: 20, right: 50, bottom: 50, left: 50 };
+  const width = 800 - margin.left - margin.right;
+  const height = 400 - margin.top - margin.bottom;
 
-  // Define the x and y scales
+  // Create the SVG element
+  const svg = d3.select("chart3")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  // Set the initial y-value to display
+  let yValue = "Administered_Cumulative";
+
+  // Function to update the line chart based on selected y-value
+  function updateChart(selectedYValue) {
+    // Update the yValue
+    yValue = selectedYValue;
+
+    // Set the y scale based on the selected y-value
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d[yValue])])
+      .range([height, 0]);
+
+    // Create the line function for the selected y-value
+    const line = d3.line()
+      .x(d => xScale(d.Date))
+      .y(d => yScale(d[yValue]));
+
+    // Remove any existing lines
+    svg.selectAll(".line").remove();
+
+    // Add the line for the selected y-value
+    svg.append("path")
+      .datum(data)
+      .attr("class", `line ${yValue}-line`)
+      .attr("d", line);
+  }
+
+  // X scale based on date
   const xScale = d3.scaleTime()
     .domain(d3.extent(data, d => d.Date))
     .range([0, width]);
 
+  // Initial y scale based on Administered_Cumulative
   const yScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d[yValue])])
+    .domain([0, d3.max(data, d => d.Administered_Cumulative)])
     .range([height, 0]);
 
-  // Define the line functions
+  // Initial line function based on Administered_Cumulative
   const line = d3.line()
     .x(d => xScale(d.Date))
-    .y(d => yScale(d[yValue]));
+    .y(d => yScale(d.Administered_Cumulative));
 
-  // Draw the line chart
-  svg.append('path')
+  // Add the initial line to the chart
+  svg.append("path")
     .datum(data)
-    .attr('class', 'line')
-    .attr('d', line);
+    .attr("class", "line administered-line")
+    .attr("d", line);
 
-  // Add x-axis
-  svg.append('g')
-    .attr('class', 'axis x-axis')
-    .attr('transform', `translate(0, ${height})`)
+  // X axis
+  svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
     .call(d3.axisBottom(xScale));
 
-  // Add y-axis
-  svg.append('g')
-    .attr('class', 'axis y-axis')
+  // Y axis
+  svg.append("g")
     .call(d3.axisLeft(yScale));
 
-  // Function to update the chart based on the selected y-value
-  function updateChart(selectedValue) {
-    yValue = selectedValue;
-
-    // Update the y scale domain
-    yScale.domain([0, d3.max(data, d => d[yValue])]);
-
-    // Update the line function
-    line.y(d => yScale(d[yValue]));
-
-    // Select the existing line and update the data
-    svg.selectAll('.line')
-      .datum(data)
-      .transition()
-      .attr('d', line);
-
-    // Update the y-axis
-    svg.select('.y-axis')
-      .transition()
-      .call(d3.axisLeft(yScale));
-  }
-
-  // Button event listeners
-  d3.select('#btnAdministered').on('click', () => updateChart('Administered_Cumulative'));
-  d3.select('#btnSeriesComplete').on('click', () => updateChart('Series_Complete_Cumulative'));
-  d3.select('#btnBooster').on('click', () => updateChart('Booster_Cumulative'));
+  // Button event listeners to update the chart
+  document.getElementById("administered-button").addEventListener("click", () => updateChart("Administered_Cumulative"));
+  document.getElementById("series-complete-button").addEventListener("click", () => updateChart("Series_Complete_Cumulative"));
+  document.getElementById("booster-button").addEventListener("click", () => updateChart("Booster_Cumulative"));
 });
